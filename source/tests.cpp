57bf55bdb6f11608299cbdf86a0f37e063317d6b
SZ <sven.zschunke@web.de>
2024-06-24 16:24:28 +0200

    Aufgabe 5.6 Implementieren der Testcases in test.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,6 +3,8 @@
 #include "sphere.hpp"
 #include "box.hpp"
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -19,6 +21,41 @@ TEST_CASE("Test Box area and volume", "[box]") {
     Box box(glm::vec3(0.0, 0.0, 0.0), glm::vec3(2.0, 3.0, 4.0));
     REQUIRE(box.area() == Approx(2 * (2.0 * 3.0 + 3.0 * 4.0 + 4.0 * 2.0)));
     REQUIRE(box.volume() == Approx(2.0 * 3.0 * 4.0));
+}
+
+//Aufgabe 5.6
+TEST_CASE("intersect_ray_sphere", "[intersect]") {
+    // Ray
+    glm::vec3 ray_origin{ 0.0f, 0.0f, 0.0f };
+    glm::vec3 ray_direction{ 0.0f, 0.0f, 1.0f }; // normalized direction
+
+    // Sphere
+    glm::vec3 sphere_center{ 0.0f, 0.0f, 5.0f };
+    float sphere_radius{ 1.0f };
+
+    float distance = 0.0f;
+    bool result = glm::intersectRaySphere(
+        ray_origin, glm::normalize(ray_direction),
+        sphere_center, sphere_radius * sphere_radius, // squared radius
+        distance
+    );
+
+    REQUIRE(result == true);
+    REQUIRE(distance == Approx(4.0f));
+}
+
+TEST_CASE("Test Sphere intersect", "[sphere_intersect]") {
+    Sphere sphere("TestSphere", "Green", glm::vec3(0.0, 0.0, 5.0), 1.0);
+    Ray ray(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 1.0));
+
+    HitPoint hit = sphere.intersect(ray);
+
+    REQUIRE(hit.hit == true);
+    REQUIRE(hit.distance == Approx(4.0f));
+    REQUIRE(hit.object_name == "TestSphere");
+    REQUIRE(hit.object_color == "Green");
+    REQUIRE(hit.intersection_point == glm::vec3(0.0, 0.0, 4.0));
+    REQUIRE(hit.intersection_direction == glm::vec3(0.0, 0.0, 1.0));
 }
 
 int main(int argc, char* argv[]) {
